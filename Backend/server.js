e2193527e4a3da795e7e57c3fbd584180dd5dfa9
charan jose <charanjoseph00@gmail.com>
2025-02-06 19:49:23 +0530

    Project structure, node server and express api setup done

---
+++ Backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const dotenv=require("dotenv");
+const {chats}=require("./data/data")
+const app = express();
+
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("Api is running succesfully");
+});
+
+app.get("/api/chats", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id",(req,res)=>{
+    const singleChat=chats.find((c)=>c._id === req.params.id)
+    res.send(singleChat);
+    
+})
+
+const port= process.env.PORT || 5000;
+
+app.listen(port, console.log(`Server is running on port ${port}`));
