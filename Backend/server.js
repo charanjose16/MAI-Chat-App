b26fc533ec6fde89b0e6d9ac791e2e00db627772
charan jose <charanjoseph00@gmail.com>
2025-02-08 18:03:46 +0530

    deployment setup added

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,7 @@ const userRoutes = require('./routes/userRoutes')
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const {notFound,errorHandler}=require('./middleware/errorMiddleware')
+const path=require('path')
 
 dotenv.config();
 connectDB();
@@ -21,10 +22,30 @@ app.use("/api/user",userRoutes)
 app.use("/api/chat",chatRoutes)
 app.use("/api/message",messageRoutes)
 
+
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
+
+
 app.use(notFound)
 app.use(errorHandler)
 
-const port= process.env.PORT || 5000;
+const port= process.env.PORT || 6000;
 
 const server =app.listen(port, console.log(`Server is running on port ${port}`));
 
