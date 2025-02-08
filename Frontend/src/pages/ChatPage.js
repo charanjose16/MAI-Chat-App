ebc6a4a1ecbee50131187c51d03a0007a7d3004b
charan jose <charanjoseph00@gmail.com>
2025-02-08 05:41:43 +0530

    chat page UI and frontend integration for chats is done

--- Frontend/src/pages/ChatPage.js
+++ Frontend/src/pages/ChatPage.js
@@ -1,28 +1,22 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import { Box } from "@chakra-ui/react";
+import MyChats from "../components/miscellaneous/MyChats";
+import { ChatState } from "../Context/ChatProvider"
+import ChatBox from "../components/miscellaneous/ChatBox";
+import SideDrawer from "../components/miscellaneous/SideDrawer";
+
 
 const ChatPage = () => {
-    const [chats,setChats]=useState([]);
-
-    const fetchChats = async ()=>{
-        const {data}=await axios.get('/api/chats')
-        setChats(data);
-        console.log(data);
-        
-        
-    }
-
-    useEffect(()=>{
-        fetchChats();
-    },[])
+ const {user}=ChatState();
 
   return (
-    <div>{
-        chats.map((chat)=>(
-            <div key={chat._id}>{chat.chatName}</div>
-        ))
-    }</div>
-  )
+    <div style={{ width: "100%" }}>
+      {user && <SideDrawer />}
+      <Box display="flex" justifyContent="space-between" w="100%" h="91.5vh" p="10px">
+        {user && <MyChats />}
+        {user && <ChatBox />}
+      </Box>
+    </div>
+  );
 }
 
 export default ChatPage
